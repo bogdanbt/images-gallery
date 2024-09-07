d56072895374b0e1e1694891f90903b814a26313
Bohdan Tarasenko <bt.tarasenko@gmail.com>
2024-09-07 14:16:16 +0200

    display and delete images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
 import Search from "./components/Search";
 import { useState } from "react";
+import ImageCard from "./components/ImageCard";
+import { Container, Row, Col } from "react-bootstrap";
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 function App() {
     const [word, setWord] = useState("");
     const [images, setImages] = useState([]);
+
     const handleSearchSubmit = (e) => {
         e.preventDefault();
         console.log(word);
@@ -17,6 +20,7 @@ function App() {
             .then((res) => res.json())
             .then((data) => {
                 setImages([data, ...images]);
+                setImages([{ ...data, title: word }, ...images]);
                 console.log(data);
                 console.log(images);
             })
@@ -24,6 +28,10 @@ function App() {
                 console.log(err);
             });
         setWord("");
+    };
+
+    const handleDeleteImage = (id) => {
+        setImages(images.filter((image) => image.id !== id));
     };
 
     return (
@@ -34,6 +42,19 @@ function App() {
                 setWord={setWord}
                 handleSubmit={handleSearchSubmit}
             />
+
+            <Container className="mt-4">
+                <Row xs={1} md={2} lg={3}>
+                    {images.map((image, i) => (
+                        <Col key={i} className="pb-3">
+                            <ImageCard
+                                image={images[i]}
+                                deleteImage={handleDeleteImage}
+                            />
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
         </div>
     );
 }
